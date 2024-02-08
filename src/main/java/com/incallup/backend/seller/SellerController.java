225365c07be36cfc9432dd13826aa70952c6c68e
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-09 00:55:16 +0530

    added get request for seller

--- src/main/java/com/incallup/backend/seller/SellerController.java
+++ src/main/java/com/incallup/backend/seller/SellerController.java
@@ -1,7 +1,14 @@
 package com.incallup.backend.seller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/seller")
 public class SellerController {
+
+    @GetMapping
+    public String hello(){return "hello";}
+
 }
