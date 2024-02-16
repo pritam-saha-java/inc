a5991f07320dd0aec99189b66cc5799454e078e3
Inc <info@inc.com>
2024-02-16 14:01:54 +0530

    changes at 14 feb deleted application-dev.yml

---
+++ src/main/java/com/incallup/backend/model/PostModel.java
@@ -0,0 +1,15 @@
+package com.incallup.backend.model;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity(name = "tbl_post")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostModel {
+}
