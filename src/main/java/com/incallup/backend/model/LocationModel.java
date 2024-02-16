57fd443976696c49b65f0aa94bc474f55e097bb0
Inc <info@inc.com>
2024-02-16 16:56:09 +0530

    Updated location model with variables and annotated entities

--- src/main/java/com/incallup/backend/model/LocationModel.java
+++ src/main/java/com/incallup/backend/model/LocationModel.java
@@ -1,4 +1,25 @@
 package com.incallup.backend.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LocationModel {
+
+    @Id
+    @Column(name = "idt_location")
+    private Integer idt_location;
+
+    @Column(name = "location_name")
+    private String location_name;
+
 }
