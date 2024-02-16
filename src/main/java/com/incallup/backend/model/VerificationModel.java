02ebce7baf03c61aa44a27f6ecd96d16583d3783
Inc <info@inc.com>
2024-02-16 17:07:36 +0530

    Updated annotated entities for verification model

--- src/main/java/com/incallup/backend/model/VerificationModel.java
+++ src/main/java/com/incallup/backend/model/VerificationModel.java
@@ -1,10 +1,25 @@
 package com.incallup.backend.model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity(name = "tbl_verification")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class VerificationModel {
 
+    @Id
+    @Column(name = "idVerification")
     private Integer idVerification;
 
+    @Column(name = "verification_type", unique = true)
     private String verification_type;
 }
