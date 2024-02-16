b08fdcc3f279ecedd197e1896bb37463fb62e14e
Inc <info@inc.com>
2024-02-16 17:01:59 +0530

    Updated promotion model with annotated entities

--- src/main/java/com/incallup/backend/model/PromotionModel.java
+++ src/main/java/com/incallup/backend/model/PromotionModel.java
@@ -1,8 +1,28 @@
 package com.incallup.backend.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity(name = "tbl_promotion")
+@ToString
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class PromotionModel {
 
+
+    @Id
+    @Column(name = "idPromotion")
     private Integer idPromotion;
 
+
+
+    @Column(name = "promotion_amount")
     private String promotion_amount;
 }
