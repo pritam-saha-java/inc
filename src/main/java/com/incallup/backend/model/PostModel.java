2d4ad85619e51606927da629c51ed600073cec8e
Inc <info@inc.com>
2024-02-16 14:06:26 +0530

    changes at 14 feb deleted application-dev.yml

--- src/main/java/com/incallup/backend/model/PostModel.java
+++ src/main/java/com/incallup/backend/model/PostModel.java
@@ -1,7 +1,9 @@
 package com.incallup.backend.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity(name = "tbl_post")
@@ -12,4 +14,21 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PostModel {
+
+
+    @Id
+    @Column(name = "idpost")
+    private Integer idpost;
+
+
+    post_title string
+    post_location_id int
+    post_views string
+    post_date datetime
+    post_contact string
+    post_age int
+    post_description string
+    post_category_id int
+    post_verification_id int
+    post_promotion_id int
 }
