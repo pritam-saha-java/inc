80f0b63b613368dc2ced6e5109a8e5616f2a39ae
Inc <info@inc.com>
2024-02-16 14:16:48 +0530

    Mapped database with post model

--- src/main/java/com/incallup/backend/model/PostModel.java
+++ src/main/java/com/incallup/backend/model/PostModel.java
@@ -20,15 +20,33 @@ public class PostModel {
     @Column(name = "idpost")
     private Integer idpost;
 
+    @Column(name = "post_title", unique = true)
+    private String post_title;
 
-    post_title string
-    post_location_id int
-    post_views string
-    post_date datetime
-    post_contact string
-    post_age int
-    post_description string
-    post_category_id int
-    post_verification_id int
-    post_promotion_id int
+    @Column(name = "post_location_id")
+    private Integer post_location_id;
+
+    @Column(name = "post_views")
+    private String post_views;
+
+    @Column(name = "datetime")
+    private String datetime;
+
+    @Column(name = "post_contact")
+    private String post_contact;
+
+    @Column(name = "post_age")
+    private Integer post_age;
+
+    @Column(name = "post_description")
+    private String post_description;
+
+    @Column(name = "post_category_id ")
+    private Integer post_category_id;
+
+    @Column(name = "post_verification_id ")
+    private Integer post_verification_id;
+
+    @Column(name = "post_promotion_id ")
+    private Integer post_promotion_id;
 }
