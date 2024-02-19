4e7a00db20aaa96e6392336efef4f4440d048b8d
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-19 20:26:06 +0530

    docs[documented Admin Service]: Modified Documents for AdminService class

---
+++ src/main/java/com/incallup/backend/service/impl/AdminService.java
@@ -0,0 +1,10 @@
+package com.incallup.backend.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * This Service should implement both {@link com.incallup.backend.service.AdminCommandService} and {@link com.incallup.backend.service.AdminQueryService} interfaces
+ * */
+@Service
+public class AdminService {
+}
