7abdf15a1ce5e1f41ee79c2482ab53d1449801cb
Inc <info@inc.com>
2024-02-20 13:49:11 +0530

    Created Repository for Category

--- src/main/java/com/incallup/backend/repository/CustomerRepository.java
+++ src/main/java/com/incallup/backend/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.incallup.backend.repository;
 
-public interface CustomerRepository {
+import com.incallup.backend.domain.Admin;
+import com.incallup.backend.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
--- src/main/java/com/incallup/backend/repository/SellerRepository.java
+++ src/main/java/com/incallup/backend/repository/SellerRepository.java
@@ -1,4 +1,10 @@
 package com.incallup.backend.repository;
 
-public interface SellerRepository {
+import com.incallup.backend.domain.Admin;
+import com.incallup.backend.domain.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller,Integer> {
 }
