03cf9913a6e61c0f76921d7c70d301fd2f34c81f
Inc <info@inc.com>
2024-02-20 13:56:23 +0530

    Created JPA for category

--- src/main/java/com/incallup/backend/repository/CategoryRepository.java
+++ src/main/java/com/incallup/backend/repository/CategoryRepository.java
@@ -1,8 +1,11 @@
 package com.incallup.backend.repository;
 
+import com.incallup.backend.domain.Admin;
+import com.incallup.backend.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
 }
