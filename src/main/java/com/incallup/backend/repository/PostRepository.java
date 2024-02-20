2082f919c7ff6bf6c713567963f7caf6b9d9c06f
Inc <info@inc.com>
2024-02-20 15:45:18 +0530

    Created JPA for Post Repository

--- src/main/java/com/incallup/backend/repository/PostRepository.java
+++ src/main/java/com/incallup/backend/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.incallup.backend.repository;
 
+import com.incallup.backend.domain.Location;
+import com.incallup.backend.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface PostRepository {
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
