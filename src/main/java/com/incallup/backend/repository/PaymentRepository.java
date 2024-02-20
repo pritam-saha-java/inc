240bb0bd8b67b2add39c3853e6c430a750806be7
Inc <info@inc.com>
2024-02-20 16:00:13 +0530

    Created repository for payment

---
+++ src/main/java/com/incallup/backend/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.incallup.backend.repository;
+
+import com.incallup.backend.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
