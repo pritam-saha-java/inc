cb2f6f796c145752918fe35fdabb1afcafc4da8a
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-20 12:58:58 +0530

    docs[documented Admin Service]: Modified Documents for AdminService class

--- src/main/java/com/incallup/backend/Application.java
+++ src/main/java/com/incallup/backend/Application.java
@@ -2,6 +2,9 @@
 package com.incallup.backend;
 
 
+import com.incallup.backend.domain.Admin;
+import com.incallup.backend.repository.AdminRepository;
+import com.incallup.backend.utility.IncallupConstants;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,9 +19,13 @@ public class Application {
 
 
 	@Bean
-	CommandLineRunner run(){
+	CommandLineRunner run(AdminRepository adminRepository){
 		return args -> {
-
+//				var admin = Admin.builder()
+//						.username(IncallupConstants.ADMIN)
+//						.password(IncallupConstants.ADMIN_PASS)
+//						.build();
+//				adminRepository.save(admin);
 		System.out.println("Hello world from command line runner");
 		};
 	}
