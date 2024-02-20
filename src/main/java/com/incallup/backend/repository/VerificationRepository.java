bce76be865cd8758dffe50845b41a591a656da0a
Inc <info@inc.com>
2024-02-20 16:10:21 +0530

    Created JPA for verification

--- src/main/java/com/incallup/backend/repository/VerificationRepository.java
+++ src/main/java/com/incallup/backend/repository/VerificationRepository.java
@@ -1,7 +1,9 @@
 package com.incallup.backend.repository;
 
+import com.incallup.backend.domain.Verification;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VerificationRepository {
+public interface VerificationRepository extends JpaRepository<Verification, Integer> {
 }
