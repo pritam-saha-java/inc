81b080f0a158576c603c0379514c899da8e36d81
Inc <info@inc.com>
2024-02-20 14:16:07 +0530

    Created JPA for Location

--- src/main/java/com/incallup/backend/repository/LocationRepository.java
+++ src/main/java/com/incallup/backend/repository/LocationRepository.java
@@ -1,8 +1,10 @@
 package com.incallup.backend.repository;
 
+import com.incallup.backend.domain.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface LocationRepository {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 }
