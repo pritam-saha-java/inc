3e0169bc3b1b72b38f6d616c02980a2131f1738f
Inc <info@inc.com>
2024-02-21 16:44:44 +0530

    Created seller service

---
+++ src/main/java/com/incallup/backend/service/impl/SellerService.java
@@ -0,0 +1,7 @@
+package com.incallup.backend.service.impl;
+
+import com.incallup.backend.service.SellerCommandService;
+import com.incallup.backend.service.SellerQueryService;
+
+public class SellerService implements SellerQueryService, SellerCommandService {
+}
