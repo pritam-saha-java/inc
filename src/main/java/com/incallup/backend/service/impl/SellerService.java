f4bbeaea683df97322a9cccf1d7fce4d411a2bd6
Inc <info@inc.com>
2024-02-21 16:46:07 +0530

    Created annotation for seller

--- src/main/java/com/incallup/backend/service/impl/SellerService.java
+++ src/main/java/com/incallup/backend/service/impl/SellerService.java
@@ -2,6 +2,11 @@ package com.incallup.backend.service.impl;
 
 import com.incallup.backend.service.SellerCommandService;
 import com.incallup.backend.service.SellerQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@RequiredArgsConstructor
 public class SellerService implements SellerQueryService, SellerCommandService {
 }
