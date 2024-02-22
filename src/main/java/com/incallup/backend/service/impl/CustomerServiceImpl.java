6c2daa641e77f6fa3deac986a5de0b4ec83562ce
Inc <info@inc.com>
2024-02-22 12:42:19 +0530

    Updated Customer Service

--- src/main/java/com/incallup/backend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/incallup/backend/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,15 @@
 package com.incallup.backend.service.impl;
 
+
+import com.incallup.backend.domain.Post;
+import com.incallup.backend.exception.ApplicationException;
+import com.incallup.backend.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerServiceImpl implements{
+
+    public Post searchByTitle(String title) throws ApplicationException{
+        String nS = CustomerRepository.searchByTitle(customer.getId()).isPresent();
+    }
 }
