ab1649a9146d5a94485735ccceea9d2507ac1a66
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-22 12:31:11 +0530

    docs[properties]: changed api docs configuration
    [version from appclication.properties has been changed]

--- src/main/java/com/incallup/backend/configuration/SwaggerConfiguration.java
+++ src/main/java/com/incallup/backend/configuration/SwaggerConfiguration.java
@@ -4,16 +4,15 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
     @Bean
-    public OpenAPI employeeManagementOpenAPI(@Value("${springdoc.version}") String appVersion) {
+    public OpenAPI employeeManagementOpenAPI() {
         var api = new OpenAPI();
-        api.info(new Info().title("Incallup").version(appVersion)
+        api.info(new Info().title("Incallup")
                 .license(new License().name("incallup"))
         );
         api.components(new Components());
