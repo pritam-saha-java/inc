0cd825cc74f57a16050ce6643c5ddb2587e7e487
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-22 11:59:48 +0530

    refactor[Application]: commandlinerunner comments removed

--- src/main/java/com/incallup/backend/Application.java
+++ src/main/java/com/incallup/backend/Application.java
@@ -2,9 +2,8 @@
 package com.incallup.backend;
 
 
-import com.incallup.backend.domain.Admin;
-import com.incallup.backend.repository.AdminRepository;
-import com.incallup.backend.utility.IncallupConstants;
+
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,13 +18,9 @@ public class Application {
 
 
 	@Bean
-	CommandLineRunner run(AdminRepository adminRepository){
+	CommandLineRunner run(){
 		return args -> {
-//				var admin = Admin.builder()
-//						.username(IncallupConstants.ADMIN)
-//						.password(IncallupConstants.ADMIN_PASS)
-//						.build();
-//				adminRepository.save(admin);
+
 		System.out.println("Hello world from command line runner");
 		};
 	}
