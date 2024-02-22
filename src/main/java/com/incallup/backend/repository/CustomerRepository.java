e356b27c1d15a8ba4945e999feaa2538781e445d
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-22 17:59:11 +0530

    customer repo improved

--- src/main/java/com/incallup/backend/repository/CustomerRepository.java
+++ src/main/java/com/incallup/backend/repository/CustomerRepository.java
@@ -5,16 +5,10 @@ import com.incallup.backend.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
-    Optional<Customer> searchByTitle(String title);
 
-    Optional<Customer> searchByCategory(String category);
-
-    Optional<Customer> searchByCategoryAndLocation(String category, String location);
-
-    Optional<Customer> searchByLocation(String location);
 }
