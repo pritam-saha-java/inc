97dafc82bed3758932cb48e2bcb4a3d91ec5bc9b
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-28 20:28:52 +0530

    feat(application): UI SellerController integration

--- src/main/java/com/incallup/backend/Application.java
+++ src/main/java/com/incallup/backend/Application.java
@@ -25,7 +25,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+//
 
 	@Bean
 	CommandLineRunner run(AdminService adminService, AdminRepository adminRepository){
