f504e89910753baa3278e28ad4feb074584fa7c7
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-03-01 20:21:08 +0530

    refactor[UI]: UI connectivity & configuration
    Signed-off-by: Kunal Dabhane <kunaldabhane20012@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 #InCallup
 
-API documentation is the [link](http://localhost:8081/swagger-ui/index.html) format : http://{base-url}/swagger-ui/index.html
+API documentation is the [link](http://localhost/swagger-ui/index.html) format : http://{base-url}/swagger-ui/index.html
 
 port number : 8081
-		
+
+fix post page footer
+
+title page
+not able to login using signup
+whatsapp telegram integration
+signup link should be domain/register
+login link should be domain/login
+
--- src/main/java/com/incallup/backend/domain/Admin.java
+++ src/main/java/com/incallup/backend/domain/Admin.java
@@ -14,7 +14,6 @@ import java.time.Instant;
 @Setter
 @ToString
 @NoArgsConstructor
-@Builder
 @AllArgsConstructor
 @Entity(name = "tbl_admin")
 public class Admin
@@ -35,12 +34,12 @@ public class Admin
 //    private Set<Role> authorities= new HashSet<>();
 
     @NotNull
-    @Column(name = "admin_username",unique = true,nullable = false)
+    @Column(name = "admin_username", unique = true, nullable = false)
     private String username;
 
     @NotNull
     @JsonIgnore
-    @Column(name = "admin_password",nullable = false)
+    @Column(name = "admin_password", nullable = false)
     private String password;
 
     @Column(name = "admin_type")
@@ -54,6 +53,61 @@ public class Admin
     @UpdateTimestamp
     @Column(name = "admin_updated_at")
     private Instant updatedAt;
+
+    public static AdminBuilder builder() {
+        return new AdminBuilder();
+    }
+
+    public static class AdminBuilder {
+        private Integer id;
+        private @NotNull String username;
+        private @NotNull String password;
+        private String type;
+        private Instant createdAt;
+        private Instant updatedAt;
+
+        AdminBuilder() {
+        }
+
+        public AdminBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public AdminBuilder username(@NotNull String username) {
+            this.username = username;
+            return this;
+        }
+
+        @JsonIgnore
+        public AdminBuilder password(@NotNull String password) {
+            this.password = password;
+            return this;
+        }
+
+        public AdminBuilder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public AdminBuilder createdAt(Instant createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public AdminBuilder updatedAt(Instant updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Admin build() {
+            return new Admin(this.id, this.username, this.password, this.type, this.createdAt, this.updatedAt);
+        }
+
+        public String toString() {
+            return "Admin.AdminBuilder(id=" + this.id + ", username=" + this.username + ", password=" + this.password + ", type=" + this.type + ", createdAt=" + this.createdAt + ", updatedAt=" + this.updatedAt + ")";
+        }
+    }
 
 
 //    @Override
