60c9c753d754a49e6fe6cf55ca84c97a9ba9f936
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-03-04 03:10:22 +0530

    refactor[UI]: staged all ui updates
    Signed-off-by: Kunal Dabhane <kunaldabhane20012@gmail.com>

--- src/main/java/com/incallup/backend/Application.java
+++ src/main/java/com/incallup/backend/Application.java
@@ -2,18 +2,18 @@
 package com.incallup.backend;
 
 
-//import com.incallup.backend.domain.*;
-//import com.incallup.backend.repository.AdminRepository;
-//import com.incallup.backend.repository.SellerRepository;
-//import com.incallup.backend.service.impl.AdminService;
+import com.incallup.backend.domain.*;
+import com.incallup.backend.repository.AdminRepository;
+import com.incallup.backend.repository.SellerRepository;
+import com.incallup.backend.service.impl.AdminService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//import java.util.ArrayList;
-//import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * @author incallup
@@ -29,76 +29,76 @@ public class Application {
 
 	@Bean
 	CommandLineRunner run(
-//			AdminService adminService
-//			, AdminRepository adminRepository
-//			, SellerRepository sellerRepository
+			AdminService adminService
+			, AdminRepository adminRepository
+			, SellerRepository sellerRepository
 	){
 		return args -> {
 
 
-//
-//
-//			var sellers = adminService.listSellers();
-//			if(sellers.isEmpty())
-//			{
-//
-//				sellerRepository.save(Seller.builder()
-////								.posts(new ArrayList<Post>())
-//						.email("sample@email.com")
-//								.id(7800)
-//						.username("seller")
-//						.password("123456")
-//						.build());
-//			}
-//
-//			var categories = adminService.listCategories();
-//			if(categories.isEmpty()){
-//
-//
-//
-//
-//				adminService.createCategory(Category.builder().title("Call Girl").build());
-//				adminService.createCategory(Category.builder().title("Verified Call Girl").build());
-//				adminService.createCategory(Category.builder().title("Spa And Massage").build());
-//				adminService.createCategory(Category.builder().title("Dating").build());
-//
-//			}
-//
-//
-//			var locations = adminService.listLocations();
-//			if(locations.isEmpty()){
-//				adminService.createLocation(Location.builder().district("kolkata").state("West Bangal").build());
-//				adminService.createLocation(Location.builder().district("digha").state("West Bangal").build());
-//				adminService.createLocation(Location.builder().district("purulia").state("West Bangal").build());
-//				adminService.createLocation(Location.builder().district("mandarmani").state("West Bangal").build());
-//				adminService.createLocation(Location.builder().district("thane").state("MAHARASHTRA").build());
-//				adminService.createLocation(Location.builder().district("pune").state("MAHARASHTRA").build());
-//				adminService.createLocation(Location.builder().district("solapur").state("MAHARASHTRA").build());
-//				adminService.createLocation(Location.builder().district("mumbai").state("MAHARASHTRA").build());
-//				adminService.createLocation(Location.builder().district("bangalore").state("Karnataka").build());
-//				adminService.createLocation(Location.builder().district("bidar").state("Karnataka").build());
-//				adminService.createLocation(Location.builder().district("ahemdabad").state("gujrat").build());
-//				adminService.createLocation(Location.builder().district("hyderabad").state("tamilnadu").build());
-//			}
-//			Optional<Admin> adminn = adminRepository.findUserByUsername("kunal");
-//            adminn.ifPresent(admin -> System.out.println("this is admin " + admin));
-////			if(roleRepository.findRoleByAuthority("ADMIN").isPresent()) {
-////				return;
-////			}
-//
-////			var adminRole = roleRepository.save(Role.builder()
-////							.authority("ADMIN")
-////					.build());
-////			var roles = new HashSet<Role>();
-////			roles.add(adminRole);
-//			if(adminRepository.findUserByUsername("kunal").isPresent())
+
+
+			var sellers = adminService.listSellers();
+			if(sellers.isEmpty())
+			{
+
+				sellerRepository.save(Seller.builder()
+//								.posts(new ArrayList<Post>())
+						.email("sample@email.com")
+								.id(7800)
+						.username("seller")
+						.password("123456")
+						.build());
+			}
+
+			var categories = adminService.listCategories();
+			if(categories.isEmpty()){
+
+
+
+
+				adminService.createCategory(Category.builder().title("Call Girl").build());
+				adminService.createCategory(Category.builder().title("Verified Call Girl").build());
+				adminService.createCategory(Category.builder().title("Spa And Massage").build());
+				adminService.createCategory(Category.builder().title("Dating").build());
+
+			}
+
+
+			var locations = adminService.listLocations();
+			if(locations.isEmpty()){
+				adminService.createLocation(Location.builder().district("kolkata").state("West Bangal").build());
+				adminService.createLocation(Location.builder().district("digha").state("West Bangal").build());
+				adminService.createLocation(Location.builder().district("purulia").state("West Bangal").build());
+				adminService.createLocation(Location.builder().district("mandarmani").state("West Bangal").build());
+				adminService.createLocation(Location.builder().district("thane").state("MAHARASHTRA").build());
+				adminService.createLocation(Location.builder().district("pune").state("MAHARASHTRA").build());
+				adminService.createLocation(Location.builder().district("solapur").state("MAHARASHTRA").build());
+				adminService.createLocation(Location.builder().district("mumbai").state("MAHARASHTRA").build());
+				adminService.createLocation(Location.builder().district("bangalore").state("Karnataka").build());
+				adminService.createLocation(Location.builder().district("bidar").state("Karnataka").build());
+				adminService.createLocation(Location.builder().district("ahemdabad").state("gujrat").build());
+				adminService.createLocation(Location.builder().district("hyderabad").state("tamilnadu").build());
+			}
+			Optional<Admin> adminn = adminRepository.findUserByUsername("kunal");
+            adminn.ifPresent(admin -> System.out.println("this is admin " + admin));
+//			if(roleRepository.findRoleByAuthority("ADMIN").isPresent()) {
 //				return;
-//			Admin admin = Admin.builder()
-////					.authorities(roles)
-//					.username("kunal")
-//					.password("kunal123")
-//					.build();
-//			adminRepository.save(admin);
+//			}
+
+//			var adminRole = roleRepository.save(Role.builder()
+//							.authority("ADMIN")
+//					.build());
+//			var roles = new HashSet<Role>();
+//			roles.add(adminRole);
+			if(adminRepository.findUserByUsername("kunal").isPresent())
+				return;
+			Admin admin = Admin.builder()
+//					.authorities(roles)
+					.username("kunal")
+					.password("kunal123")
+					.build();
+			adminRepository.save(admin);
 		System.out.println("Hello world from command line runner");
 
 		};
