a56c08c1aee139fc1fc491959ba2284a8ec15a7c
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-03-19 15:59:43 +0530

    refactor[UI]: removed uncommented code from CustomerServiceImpl related to decompression
    Signed-off-by: Kunal Dabhane <kunaldabhane20012@gmail.com>

--- src/main/java/com/incallup/backend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/incallup/backend/service/impl/CustomerServiceImpl.java
@@ -66,24 +66,7 @@ public class CustomerServiceImpl implements CustomerService {
     public String convertByteArrayToBase64(byte[] byteArray) {
         return Base64.getEncoder().encodeToString(byteArray);
     }
-//    public  byte[] decompress(byte[] compressedData)  {
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(compressedData);
-//        Inflater inflater = new Inflater();
-//        byte[] buffer = new byte[1024];
-//        try {
-//            inflater.setInput(compressedData);
-//            while (!inflater.finished()) {
-//                int count = inflater.inflate(buffer);
-//                outputStream.write(buffer, 0, count);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            inflater.end();
-//        }
-//        return outputStream.toByteArray();
-//    }
+
 
     @Override
     public List<Post> searchByCategoryAndLocation(String categoryString, String locationString) throws ApplicationException{
