bc60cb0bf3b1ec3efb35c7101c7abf549a761301
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-03-28 18:54:35 +0530

    refactor[UI]: Desktop Opening code removed
    Signed-off-by: Kunal Dabhane <kunaldabhane20012@gmail.com>

--- src/main/java/com/incallup/backend/Application.java
+++ src/main/java/com/incallup/backend/Application.java
@@ -12,11 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 
 /**
  * @author incallup
@@ -29,30 +24,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-//
+
 
 	@Bean
 	protected CommandLineRunner run(){
 		return args -> {
 
 
-			String url = "https://localhost";
-			if(Desktop.isDesktopSupported()){
-				Desktop desktop = Desktop.getDesktop();
-				try {
-					desktop.browse(new URI(url));
-				} catch (IOException | URISyntaxException e) {
-					e.printStackTrace();
-				}
-			}
-//			else{
-//				Runtime runtime = Runtime.getRuntime();
-//				try {
-//					runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			}
 
 
 		};
